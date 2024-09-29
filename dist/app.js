5c01df2b511b03df66a2feeabe3c000ea1427b97
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-09-29 10:33:30 +0900

    Main.elm を変更して、app.js を更新

--- dist/app.js
+++ dist/app.js
@@ -9951,7 +9951,7 @@ var $author$project$Main$viewASTRoot = F2(
 						$elm$json$Json$Decode$succeed($author$project$Main$MsgDblClick))),
 					A2(
 					$author$project$Main$preventDefaultOn,
-					'Duplicate',
+					'touchstart',
 					A2(
 						$author$project$Main$whenNotDragging,
 						model,
@@ -10236,7 +10236,7 @@ var $author$project$Main$view = function (model) {
 										_List_fromArray(
 											[
 												A2($elm$html$Html$Attributes$style, 'width', '150px'),
-												$elm$html$Html$Attributes$placeholder('マーカス'),
+												$elm$html$Html$Attributes$placeholder('新しい関数名'),
 												$elm$html$Html$Attributes$value(model.routineBox),
 												$elm$html$Html$Attributes$hidden(false),
 												A2(
