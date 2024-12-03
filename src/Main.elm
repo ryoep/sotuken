e78680628cbf6337d554aacbbd3a009f4b4b8d4a
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-12-03 10:12:58 +0900

    Main.elm を変更して、app.js を更新

--- src/Main.elm
+++ src/Main.elm
@@ -1917,7 +1917,7 @@ view model =
                     []
                     [ input
                         [ style "width" "150px"
-                        , placeholder "ラッシュフォード" --新しい関数名
+                        , placeholder "新しい関数名" --新しい関数名
                         , value model.routineBox
                         , hidden False
                         , (Decode.map MsgRoutineBoxChanged targetValue) |> on "input"
